f1d861850d0e95d8ce6510679d32847243ccd728
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-08-11 12:34:07 +0200

    Backend almost finished but problems/errors still to fix

--- middlewares/heroes.js
+++ middlewares/heroes.js
@@ -30,10 +30,38 @@ const verifyAvailability = (req, res, next) => {
 }
 
 
+const validateHero = (req, res, next) => {
+  const { slug } = req.params
+  const heroe = heroes.find(heroe => heroe.slug === slug)
+  const heroeIndex = heroes.findIndex(heroe => heroe.slug === slug)
+
+  if (heroe)
+  & (hero.slug.typeOf === "string")
+  & (hero.name.typeOf === "string")
+  & (hero.power.typeOf === "string")
+  & (hero.color.typeOf === "string")
+  & (hero.isAlive.typeOf === "boolean")
+  & (hero.age.typeOf === "rnumber")
+  & (hero.image.typeOf === "string")
+  {
+    req.heroe = heroe
+    req.userIndex = heroeIndex
+    next()
+  } else {
+    res.status(400).json(`Les données entrées ne sont pas toutes compatibles`)
+      next()
+    }
+}
+
+
 module.exports = {
   verifyHeroe: verifyHeroe
 }
 
 module.exports = {
   verifyAvailability: verifyAvailability
+}
+
+module.exports = {
+  validateHero: validateHero
 }
--- routes/heroes.js
+++ routes/heroes.js
@@ -8,7 +8,7 @@ const { verifyAvailability } = require('../middlewares/heroes')
 
 app.get('/', (req, res, next) => {
   res.json(heroes)
-  res.send('Ls voici tous...')
+  res.send('Les voici tous...')
   next()
 })
 
@@ -23,6 +23,27 @@ app.post('/:slug', verifyAvailability, (req, res, next) => {
   res.send('Et un super héros de plus, un!')
   next()
 })
+
+app.put('/:slug',verifyAvailability, validateHero, (req, res, next) => {
+  const { slug } = req.params
+  const heroe = heroes.find(heroe => heroe.slug === slug)
+  const heroeIndex = heroes.findIndex(heroe => heroe.slug === slug)
+
+  heroe.slug = req.body.slug,
+  heroe.name = req.body.name,
+  heroe.power = req.body.power,
+  heroe.color = req.body.color,
+  heroe.isAlive = req.body.isAlive,
+  heroe.age = req.body.age,
+  heroe.image = req.body.image
+  res.status(200).json(heroe)
+
+next()
+})
+
+
+
+
 
 app.delete('/:slug', verifyHeroe, (req, res, next) => {
   heroes.splice(req.heroeIndex,1)
@@ -42,15 +63,15 @@ app.post('/:slug/powers', verifyAvailability, (req, res, next) => {
   next()
 })
 
-app.put('/:slug/powers', verifyAvailability, (req, res, next) => {
+app.put('/:slug/powers/:power', verifyAvailability, (req, res, next) => {
   res.push(req.hero.power)
   res.send('Et un nouveau pouvoir!')
   next()
 })
 
-app.delete('/:slug/powers', verifyHeroe, (req, res, next) => {
+app.delete('/:slug/powers/:power', verifyHeroe, (req, res, next) => {
   heroes.power.splice(req.heroeIndex,1)
-  res.json(`${slug} effacé!`)
+  res.json(`Le pouvoir ${power} de ${slug} effacé!`)
   next()
 })
 
